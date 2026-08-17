45338371f3f9b3e47bd95ee38e7d61769a37f9f3
korvo <128074105+corvid3@users.noreply.github.com>
2026-08-17 09:12:24 -0400

    fix: bootstrap bug

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ if [ ! -d $HOME/.hewg/bootstrap/crow.scl ]; then
 fi
 
 
-g++ -I $HOME/.hewg/bootstrap -std=c++23 -Wall -Wextra -O2 src/scl.cc -c -o scl.o
+g++ -I $HOME/.hewg/bootstrap -I./include-std=c++23 -Wall -Wextra -O2 src/scl.cc -c -o scl.o
 ar rcs libscl.a scl.o
 
 cp include/scl.hh $HOME/.hewg/bootstrap/crow.scl/
